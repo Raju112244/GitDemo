2aa710105524b71387afc68704484c5b1e8cc9d1
raju <sankatiraju017@gmail.com>
2025-07-24 20:45:44 +0530

    This is commit from Asain time zone guy

---
+++ src/Raju/RestApi.java
@@ -0,0 +1,24 @@
+package Raju;
+
+import org.testng.annotations.Test;
+
+public class RestApi 
+{
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+
+
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTiwtter");
+	}
+	
+}
